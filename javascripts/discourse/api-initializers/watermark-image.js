6675ae8bc59b838fa71f4b90fb2c6a973b35ae46
Arkshine <github@arkshine.me>
2024-12-26 04:14:12 +0100

    DEV: Add categories and  groups settings

--- javascripts/discourse/api-initializers/watermark-image.js
+++ javascripts/discourse/api-initializers/watermark-image.js
@@ -1,19 +1,51 @@
 import { setOwner } from "@ember/owner";
+import { service } from "@ember/service";
 import { withPluginApi } from "discourse/lib/plugin-api";
 import { imageDataToFile } from "../lib/media-watermark-utils";
 import UppyMediaWatermark from "../lib/uppy-media-watermark-plugin";
 import Watermark from "../lib/watermark";
 
 class WatermarkInit {
+  @service currentUser;
+
   constructor(owner) {
     setOwner(this, owner);
 
     withPluginApi("1.8.0", (api) => {
       api.addComposerUploadPreProcessor(
         UppyMediaWatermark,
-        ({ isMobileDevice }) => {
+        ({ composerModel, isMobileDevice }) => {
           return {
             watermarkFn: async (file) => {
+              if (owner.isDestroyed || owner.isDestroying) {
+                return Promise.resolve();
+              }
+
+              if (
+                settings.watermark_categories &&
+                !settings.watermark_categories
+                  .split("|")
+                  .map((c) => Number(c))
+                  .includes(composerModel.categoryId)
+              ) {
+                return Promise.resolve();
+              }
+
+              if (settings.watermark_groups.length > 0) {
+                const requiredGroups = settings.watermark_groups
+                  .split("|")
+                  .map((g) => Number(g));
+
+                if (
+                  !requiredGroups.includes(0) &&
+                  !this.currentUser.groups
+                    .map((group) => group.id)
+                    .some((group) => requiredGroups.includes(group))
+                ) {
+                  return Promise.resolve();
+                }
+              }
+
               const watermark = new Watermark(file.data);
               const imageData = await watermark.sendToWorker();
 
